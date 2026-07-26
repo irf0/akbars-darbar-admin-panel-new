9a26a9bcea5442fa3082f698fe8eb65ecc2882f7
irf0 <im4941668@gmail.com>
2026-07-26 20:43:36 +0530

    chore(branding): update application branding and icons

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,18 +6,19 @@ import icon from '../../resources/icon.png?asset';
 app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 
 function createWindow(): void {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false,
     },
   });
+
+  mainWindow.maximize();
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show();
